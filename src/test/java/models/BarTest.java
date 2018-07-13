73d76bc1690f19e4bf0d22224c21f4cc531866c6
Garnett Puett <gpuett@gmail.com>
2018-07-13 09:17:53 -0700

    set name test

--- src/test/java/models/BarTest.java
+++ src/test/java/models/BarTest.java
@@ -12,6 +12,15 @@ public class BarTest {
         assertEquals("Kelly's Olympian", bar.getName());
     }
 
+    @Test
+    public void setName() {
+        Bar bar = setupBar();
+        bar.setName("C Bar");
+        assertEquals("C Bar", bar.getName());
+    }
+
+
+
     //helper model
     public Bar setupBar() {
         return new Bar("Kelly's Olympian", "426 SW Washington St, Portland, OR 97204", "503-228-3669", "$1 off draft beer, well drinks and wine", "4pm-7pm");
